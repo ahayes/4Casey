fabcd50e6c8153c04a7c78cd8ee09093ca4f93e2
Amos Hayes <ahayes@polkaroo.net>
2013-09-30 02:23:47 -0400

    gpio version 1.0

--- gpio_audio_control.py
+++ gpio_audio_control.py
@@ -1,44 +1,73 @@
 #!/usr/bin/python3
 
-
+import os
 import subprocess
 import time
 import atexit
 import RPi.GPIO as GPIO
 
+devnull = open('/dev/null', 'w')
 procs = []
-channel0 = 11
-channel1 = 13
+pin11 = 11
+pin13 = 13
+pin15 = 15
+pin16 = 16
+pin22 = 22
 
 def play_sound0(event):
-	procstmp = procs
-	for proc in procstmp:
+	for proc in list(procs):
 		proc.terminate()
 		procs.remove(proc)
-	sound0 = subprocess.Popen(["/usr/bin/aplay", "piano2.wav"],shell=False,stdin=None,stdout=None,stderr=None)
+	sound0 = subprocess.Popen(["/usr/bin/aplay", "Rambo.wav"],shell=False,stdin=None,stdout=devnull,stderr=devnull)
 	procs.append(sound0)
 
 def play_sound1(event):
-	procstmp = procs
-	for proc in procstmp:
+	for proc in list(procs):
 		proc.terminate()
 		procs.remove(proc)
-	sound1 = subprocess.Popen(["/usr/bin/aplay", "organfinale.wav"],shell=False,stdin=None,stdout=None,stderr=None)
+	sound1 = subprocess.Popen(["/usr/bin/aplay", "runaway.wav"],shell=False,stdin=None,stdout=devnull,stderr=devnull)
 	procs.append(sound1)
 
+def play_sound2(event):
+	for proc in list(procs):
+		proc.terminate()
+		procs.remove(proc)
+	sound2 = subprocess.Popen(["/usr/bin/aplay", "Borat_Yes.wav"],shell=False,stdin=None,stdout=devnull,stderr=devnull)
+	procs.append(sound2)
+
+def play_sound3(event):
+	for proc in list(procs):
+		proc.terminate()
+		procs.remove(proc)
+	sound3 = subprocess.Popen(["/usr/bin/aplay", "Borat_No.wav"],shell=False,stdin=None,stdout=devnull,stderr=devnull)
+	procs.append(sound3)
+
+def shutdown_os(event):
+	print ("Shutting down...")
+	os.system("shutdown -h now &")
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(channel0, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(channel1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(channel0, GPIO.RISING, callback=play_sound0, bouncetime=1000)
-GPIO.add_event_detect(channel1, GPIO.RISING, callback=play_sound1, bouncetime=1000)
+GPIO.setup(pin11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(pin11, GPIO.FALLING, callback=play_sound0, bouncetime=500)
+GPIO.add_event_detect(pin13, GPIO.FALLING, callback=play_sound1, bouncetime=500)
+GPIO.add_event_detect(pin15, GPIO.FALLING, callback=play_sound2, bouncetime=500)
+GPIO.add_event_detect(pin16, GPIO.FALLING, callback=play_sound3, bouncetime=500)
+GPIO.add_event_detect(pin22, GPIO.FALLING, callback=shutdown_os, bouncetime=2000)
 
 while True:
-    pass
+    time.sleep(1)
 
 @atexit.register
 def kill_subprocesses():
 	for proc in procs:
 		proc.kill()
 	GPIO.cleanup()
-	GPIO.remove_event_detect(channel0)
-	GPIO.remove_event_detect(channel1)
+	GPIO.remove_event_detect(pin11)
+	GPIO.remove_event_detect(pin13)
+	GPIO.remove_event_detect(pin15)
+	GPIO.remove_event_detect(pin16)
+	GPIO.remove_event_detect(pin22)
